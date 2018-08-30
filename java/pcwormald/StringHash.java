03360cc27e98763b21fa8fa02a9d320e6c0d36e8
888pcw <888pcw@b888.mailc.net>
2018-08-30 15:05:56 +0100

    add java source

---
+++ java/pcwormald/StringHash.java
@@ -0,0 +1,23 @@
+package pcwormald;
+
+//modified from source https://www.infoq.com/articles/Graal-Java-JIT-Compiler
+
+public final class StringHash {
+
+  public static void main(String[] args) {
+
+    for (int i=1; i<2_000_000_000; i+=1_000_000) {
+      final long now = System.nanoTime();
+      char[] chars = new char[i];
+      java.util.Arrays.fill(chars,'x');
+      String str = new String(chars);
+      str.hashCode();
+      System.out.println("Length: "+ i +" took: "+ (System.nanoTime() - now) +" ns");
+    }
+
+
+  }
+}
+
+
+
